872fd9bfa9ffdacf1370501364c2aa8803e0a55c
Chanawatn Nakwangsarth <chanawatnpound@gmail.com>
2018-06-02 07:29:19 +0700

    first commit

--- Angular_4_Seed_Project/src/app/app.helpers.ts
+++ Angular_4_Seed_Project/src/app/app.helpers.ts
@@ -16,31 +16,31 @@ export function correctHeight() {
   var wrapperHeight = pageWrapper.height();
 
   if (navbarHeight > wrapperHeight) {
-    pageWrapper.css("min-height", navbarHeight + "px");
+    pageWrapper.css('min-height', navbarHeight + 'px');
   }
 
   if (navbarHeight <= wrapperHeight) {
     if (navbarHeight < jQuery(window).height()) {
-      pageWrapper.css("min-height", jQuery(window).height() + "px");
+      pageWrapper.css('min-height', jQuery(window).height() + 'px');
     } else {
-      pageWrapper.css("min-height", navbarHeight + "px");
+      pageWrapper.css('min-height', navbarHeight + 'px');
     }
   }
 
   if (jQuery('body').hasClass('fixed-nav')) {
     if (navbarHeight > wrapperHeight) {
-      pageWrapper.css("min-height", navbarHeight + "px");
+      pageWrapper.css('min-height', navbarHeight + 'px');
     } else {
-      pageWrapper.css("min-height", jQuery(window).height() - 60 + "px");
+      pageWrapper.css('min-height', jQuery(window).height() - 60 + 'px');
     }
   }
 }
 
 export function detectBody() {
   if (jQuery(document).width() < 769) {
-    jQuery('body').addClass('body-small')
+    jQuery('body').addClass('body-small');
   } else {
-    jQuery('body').removeClass('body-small')
+    jQuery('body').removeClass('body-small');
   }
 }
 
